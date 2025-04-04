d2296a3f58ce3e8df61f85547ab9a55d5634d745
wsy998 <489402539@qq.com>
2025-04-04 22:58:16 +0800

    add sass

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -2,7 +2,6 @@ import { vuePlugin, vueDevToolsPlugin } from './vue'
 import { jsxPlugin } from './jsx'
 import { AutoImportPlugin, ComponentsPlugin, IconsPlugin, routerPlugin } from './unplugin'
 import { UnocssPlugin } from './uno'
-import { AlovaJs } from './alova'
 
 export default () => {
   return [
